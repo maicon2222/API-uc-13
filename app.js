9430fdedf172d833b83a2cc326605252cb0d56bf
MAICON PINHEIRO VENANCIO <maicon.pvenancio@senacsp.edu.br>
2025-01-30 13:50:38 -0300

    Aula-31-01

--- app.js
+++ app.js
@@ -1,54 +1,90 @@
-// importando nossa tabela
 import carros2025 from "./tabelaCarros.js";
-
-//importa o modulo express
+ 
 import express from "express";
-
-import { modeloCarro } from "./validacao.js";
-
-//criando a variavel app do tipo express
+ 
+import { modeloCarro, modeloAtualizacaoCarro } from "./validacao.js";
+ 
 const app = express();
-
-//indica queusaremos um arquivo do ti´po json
+ 
+// Configura o Expres para entender requisições com o corpo em formato JSON
 app.use(express.json());
-
+ 
+// Define uma rota GET no caminho '/' que irá retornar a lista completa de carros
 app.get("/", (requisicao, resposta) => {
-  resposta.status(200).send(carros2025); //restora a lista de carro com string
+  // Quando a rota dor acessada, a resposta te´ra p código de status  200(ok) e enviará a lista de carris
+  resposta.status(200).send(carros2025); // Retorna lista de carros com status 200
 });
-
+ 
 app.get("/:sigla", (req, res) => {
-  const siglaInformada = req.params.sigla.toUpperCase(); // obter a sigla e deixa maiuscula
+  const siglaInformada = req.params.sigla.toUpperCase(); // Obtém a sigla e deixa Maiuscula
   const carro = carros2025.find(
-    (infocarro) => infocarro.sigla === siglaInformada
-  );
+    (infoCarro) => infoCarro.sigla === siglaInformada
+  ); // Busca o carro pela sigla
   if (!carro) {
+    // Se o carro não for encontrado retorna erro 404.
     res.status(404).send("Não existe carro com a sigla informada!");
     return;
   }
-  res.status(200).send(carro);
+  res.status(200).send(carro); // Se encontrado retorna o carro e status 200
 });
-
-app.post('/', (req, res) => {
-  const novoCarro = req.body; // obtem o novo carro  e status 200
-  const { error} = modeloCarro.validate(novoCarro); //valida os dados do novo carro.
+ 
+app.post("/", (req, res) => {
+  const novoCarro = req.body; // Obtém o novo carro enviado no corpo da requisição
+  const { error } = modeloCarro.validate(novoCarro); // Valida os dados do novo carro.
   if (error) {
-    // se houver erro de validação, retorna erro 400
-<<<<<<< HEAD
-    res.status(201).send(error);
-=======
+    // Se houver erro de validação, retorna erro 400
     res.status(400).send(error);
->>>>>>> d34ca1862c9d971f4e7c0ed86a67333cc15cf062
     return;
-
   }
-  carros2025.push(novoCarro); // Adiciona o carro a lista de carros 
-  res.status(200).send(novoCarro); // retorna o carro adicionado
+  carros2025.push(novoCarro); // adiciona o carro a lista de carros.
+  res.status(200).send(novoCarro); // Retorna o carro adiciona com status 200
 });
-
-// defina a porta do servidor
+ 
+app.put("/:sigla", (req, res) => {
+  const siglaInformada = req.params.sigla.toUpperCase();
+  const carroSelecionado = carros2025.find((c) => c.sigla === siglaInformada);
+  if (!carroSelecionado) {
+    // Se o carro não for encontrado retorna erro 404 e o  201 corigido
+    res.status(404).send("Não existe um carro com a sigla informada");
+    return;
+  };
+  // Valida os dados da requisição com o modelo da atualização:
+  const { error } = modeloAtualizacaoCarro.validate(req.body);
+  if (error) {
+    // Se houver erro de validação retorna erro 400
+    res.status(400).send(error);
+    return;
+  }
+  const campos = Object.keys(req.body);
+  for (let campo of campos) {
+    carroSelecionado[campo] = req.body[campo];
+  }
+  res.status(200).send(carroSelecionado);
+});
+ 
+// define a porta do servidor
 app.listen(3000, () => {
-  console.log("servidor rodando com sucesso na porta 3000");
+  console.log("servidor rodando na porta 3000");
 });
-
-// executa o app
+ 
+app.delete("/:sigla", (req, res) => {
+    const siglaInformada = req.params.sigla.toUpperCase();
+    const indiceCarro = carros2025.findIndex( (c) => c.sigla === siglaInformada);
+    if (indiceCarro === -1) {
+        // se o carro não for encontrado (indice -1), retorna Erro 404
+        res.status(404).send("Não existe um carro com a sigla informada!");
+        return;
+    }
+    const carroRemovido = carros2025.splice(indiceCarroSelecionado, 1); // Remove o carro da lista
+    res.status(200).send(carroRemovido); // Retorna o carro removido  com status 200
+});
+     
+ 
+ 
+   
+   
+// Executa o app
 // node app.js
+// Pelo nodemon
+// npx nodemon app.js
+ 
