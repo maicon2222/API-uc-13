e08322267573f5d07771a942d23f77e977d4d45f
MAICON PINHEIRO VENANCIO <maicon.pvenancio@senacsp.edu.br>
2025-01-30 14:07:10 -0300

    Aula-30-01

--- validacao.js
+++ validacao.js
@@ -26,9 +26,20 @@ export const modeloCarro = Joi.object({
  
 // Validação para atualização de carro
 export const modeloAtualizacaoCarro = Joi.object({
-  nome: Joi.string().min(3), // nome do carro, pelo menos 3 caracteres
-  sigla: Joi.string().length(3), // Sigla ou medelo, 3 caracteres
-  velocidadeMaxima: Joi.number().min(1), // Potência minima e 1 Cv
-  potencia: Joi.number().min(1), // Velocidade minima de 1km/h
-  consumo: Joi.number().min(0.1), // Ano de fabricação
-});
+  nome: Joi.string().min(3).messages({
+    'string.min': 'O nome do carro deve ter pelo menos 3 caracteres.',
+  }), // nome do carro, pelo menos 3 caracteres
+  sigla: Joi.string().length(3).messages({
+    'string.length': 'A sigla deve ter exatamente 3 caracteres.',
+  }), // Sigla ou medelo, 3 caracteres
+  velocidadeMaxima: Joi.number().min(1).message({
+    'number.min': 'A velocidade maxima deve ser maior ou igual a 1.',
+  }),
+ // Potência minima e 1 Cv
+  potencia: Joi.number().min(1).messages({
+    'number.min': 'A potencia deve ser maior ou igual a 1.',
+  }), // Velocidade minima de 1km/h
+  consumo: Joi.number().min(0.1).messages({
+    'number.min': 'O consumo deve ser maior ou igual a 1.',
+  }), // Ano de fabricação
+}).min(1);
