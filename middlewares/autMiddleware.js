1e1ca1ce79e0782daa845204a858c1aa8b2f9b02
maicon2222 <maiconpinheirov3@gmail.com>
2025-02-11 01:49:37 -0300

    11/02

--- middlewares/autMiddleware.js
+++ middlewares/autMiddleware.js
@@ -1,31 +1,28 @@
 // Importando o pacote JWT para manipulação de tokens
-import jwt from 'jsonwebbtoken';
+import jwt from 'jsonwebtoken';
 
 // Chave para verificar e criar tokens JWT
 const JWT_SECRET = 'secreta-chave';
 
-// Middleware de autenticação para verificar o token JWT nas reqisições 
+// Middleware de autenticação para verificar o token JWT nas requisições
 export const authenticate = (req, res, next) => {
-// Obtendo o token do cabeçalho authorization (token deve ser no formato " bearer <token>")
+    // Obtendo o token do cabeçalho authorization (token deve ser no formato "Bearer <token>")
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
-        return res.status(401).json({ mensagem: "Token não fornecido!" }); 
+        return res.status(401).json({ mensagem: "Token não fornecido!" });
     }
 
     try {
-    // A função jwt.verify decodifica e valida o token 
-    const decoded = jwt.verify(token, JWT_SECRET),
+        // A função jwt.verify decodifica e valida o token
+        const decoded = jwt.verify(token, JWT_SECRET);
 
-    // Se o token for valido irá decodificar
-    req.user = decoded;
+        // Se o token for válido, irá decodificar
+        req.user = decoded;
 
-    // Chama o proximo 
-    next();
-}   catch (error) {
-    res.status(401).json({ mensagem: "Token invalido"});
-}
-}
-
-
-// npm install bcrypt
+        // Chama o próximo middleware
+        next();
+    } catch (error) {
+        res.status(401).json({ mensagem: "Token inválido" });
+    }
+};
